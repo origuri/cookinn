3f42a59dfeb1b9037cf4b4b797d320d8a96bd9ae
origuri <dlcksrb7@naver.com>
2023-08-17 18:43:15 +0900

    2023.8.17 - 프렌차이즈 발주 시스템 프로젝트 cookinn 시작

---
+++ src/main/java/com/example/cookinn/CookinnApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookinnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookinnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookinn/CookinnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookinnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
